c8d92f68415a0502ff436915aaa9724a015528be
Sasorishi <on_alban@yahoo.fr>
2025-02-22 02:13:24 +0100

    feat: protected routes

--- app/front/src/main.jsx
+++ app/front/src/main.jsx
@@ -1,10 +1,12 @@
 import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import './index.css'
-import App from './App.jsx'
-import LoginPage from './pages/Login.jsx'
-import RegisterPage from './pages/Register.jsx'
+import '@/css/index.css'
+import ProtectedRoute from '@/utils/ProtectedRoute'
+import App from '@/pages/App.jsx'
+import LoginPage from '@/pages/Login.jsx'
+import RegisterPage from '@/pages/Register.jsx'
+import DashboardPage from '@/pages/Dashboard'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -13,6 +15,9 @@ createRoot(document.getElementById('root')).render(
         <Route path="/main" element={<App />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route element={<ProtectedRoute />}>
+          <Route path="/dashboard" element={<DashboardPage />} />
+        </Route>
       </Routes>
     </Router>
   </StrictMode>,
--- app/front/src/utils/axios.js
+++ app/front/src/utils/axios.js
@@ -7,4 +7,17 @@ const axiosInstance = axios.create({
   withCredentials: true, // Assurer que les cookies de session sont envoyés avec chaque requête
 })
 
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
 export default axiosInstance
